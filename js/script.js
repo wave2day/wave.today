0d3983f7e556f45f7c5f2ef17e531fd8b49ba403
wave2day <wave@wave.today>
2026-02-10 10:01:55 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// video autoplay
+const bg = document.getElementById("bgVideo");
+if (bg) {
+  bg.play().catch(()=>{});
+}
+
+// share logika
+const orb = document.getElementById("shareOrb");
+const sheet = document.getElementById("wtActionSheet");
+const backdrop = document.getElementById("wtSheetBackdrop");
+
+if (orb && sheet && backdrop) {
+
+  function openSheet() {
+    sheet.classList.add("open");
+    backdrop.classList.add("open");
+  }
+
+  function closeSheet() {
+    sheet.classList.remove("open");
+    backdrop.classList.remove("open");
+  }
+
+  orb.addEventListener("click", () => {
+    sheet.classList.contains("open") ? closeSheet() : openSheet();
+  });
+
+  backdrop.addEventListener("click", closeSheet);
+}
